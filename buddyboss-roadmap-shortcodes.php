b1f997c6715313f68e5f4e304bd283bc5468fe73
Gourav Shah <gs@initcron.org>
2024-01-04 17:01:09 +0530

    fetch the value of challenge from bb profile

--- buddyboss-roadmap-shortcodes.php
+++ buddyboss-roadmap-shortcodes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: BB Roadmaps Shortcodes  Plugin
 Description: A plugin to display the BuddyBoss 'Roadmap' and 'Milestone'  profile field.
-Version: 1.1.0
+Version: 1.2.0
 Author: Gourav Shah
 */
 
@@ -26,6 +26,16 @@ function get_bb_milestone_value() {
     ));
 }
 
+function get_bb_challenge_value() {
+    $user_id = get_current_user_id();
+    $field = 'Challenge'; // Replace with the actual field identifier
+
+    return bp_get_profile_field_data(array(
+        'field'   => $field,
+        'user_id' => $user_id
+    ));
+}
+
 function bb_roadmap_shortcode_handler($atts) {
     $roadmap_value = get_bb_roadmap_value();
     return esc_html($roadmap_value);
@@ -36,6 +46,12 @@ function bb_milestone_shortcode_handler($atts) {
     return esc_html($milestone_value);
 }
 
+function bb_challenge_shortcode_handler($atts) {
+    $challenge_value = get_bb_challenge_value();
+    return esc_html($challenge_value);
+}
+
 add_shortcode('bb_roadmap', 'bb_roadmap_shortcode_handler');
 add_shortcode('bb_milestone', 'bb_milestone_shortcode_handler');
+add_shortcode('bb_challenge', 'bb_challenge_shortcode_handler');
 
